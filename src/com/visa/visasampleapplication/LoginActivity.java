d51cd6348778d8f75ffb7225eb387557481d8759
Tricia Fu <triciasfu@gmail.com>
2014-06-30 17:29:11 -0700

    code cleaned and swipe card not done yet

--- src/com/visa/visasampleapplication/LoginActivity.java
+++ src/com/visa/visasampleapplication/LoginActivity.java
@@ -9,11 +9,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.net.wifi.WifiManager;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
-import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.Gravity;
@@ -27,26 +25,10 @@ import android.view.ViewGroup;
 import android.view.View.OnTouchListener;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-//import net.authorize.android.AuthNetActivityBase;
-//import net.authorize.android.SimpleActivity;
-//import net.authorize.auth.PasswordAuthentication;
-//import net.authorize.data.mobile.MobileDevice;
-//import net.authorize.auth.SessionTokenAuthentication;
-//import net.authorize.data.mobile.MobileDevice;
-//import net.authorize.util.StringUtils;
-//import net.authorize.xml.MessageType;
-
-
-
-
-
-
 import android.widget.Toast;
-
-import java.util.ArrayList;
-
 import net.authorize.Environment;
 import net.authorize.Merchant;
 import net.authorize.auth.PasswordAuthentication;
@@ -55,9 +37,10 @@ import net.authorize.data.mobile.MobileDevice;
 import net.authorize.mobile.TransactionType;
 import net.authorize.mobile.Transaction;
 import net.authorize.mobile.Result;
-/**
- * Activity which displays a login screen to the user. */
-public class LoginActivity extends Activity { //change to AuthNetActivityBase
+
+
+/** Activity which displays a login screen to the user. */
+public class LoginActivity extends Activity {
 
     /** The default email to populate the email field with. */
     public static final String EXTRA_LOGINID = "com.example.android.authenticatordemo.extra.LOGINID";
@@ -75,10 +58,13 @@ public class LoginActivity extends Activity { //change to AuthNetActivityBase
     private View mLoginFormView;
     private View mLoginStatusView;
     private TextView mLoginStatusMessageView;
+
+    /** Buttons. */
+    private Button loginButton;
     
     /** Strings related to login authentication. */
     private String deviceNumber = "";
-    protected static String deviceID = "358347040811237";//"359691043853624";
+    protected static String deviceID = "358347040811237"; //"359691043853624";
     private String deviceInfo = "";
     protected static Merchant _merchant = null;
 
@@ -86,19 +72,24 @@ public class LoginActivity extends Activity { //change to AuthNetActivityBase
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        /** Setup the login form */
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         setupUI(findViewById(R.id.login_form));
         getActionBar().show();
 
-        // Set up the login form.
-        mLoginID = getIntent().getStringExtra(EXTRA_LOGINID);
         mLoginIDView = (EditText) findViewById(R.id.loginID);
+        mPasswordView = (EditText) findViewById(R.id.password);
+        mLoginStatusMessageView = (TextView) findViewById(R.id.login_status_message);
+        mLoginFormView = findViewById(R.id.login_form);
+        mLoginStatusView = findViewById(R.id.login_status);
+
+        loginButton = (Button) findViewById(R.id.log_in_button);
+
+        mLoginID = getIntent().getStringExtra(EXTRA_LOGINID);
         mLoginIDView.setText(mLoginID);
 
-        mPasswordView = (EditText) findViewById(R.id.password);
-
-        // respond correlated to pressing return on the key pad and logging in
+        /** Respond to Return on keypad. */
         mPasswordView
                 .setOnEditorActionListener(new TextView.OnEditorActionListener() {
                     @Override
@@ -112,12 +103,8 @@ public class LoginActivity extends Activity { //change to AuthNetActivityBase
                     }
                 });
 
-        mLoginFormView = findViewById(R.id.login_form);
-        mLoginStatusView = findViewById(R.id.login_status);
-        mLoginStatusMessageView = (TextView) findViewById(R.id.login_status_message);
-
-        // respond to login button
-        findViewById(R.id.log_in_button).setOnClickListener(
+        /** Respond to login button. */
+        loginButton.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
@@ -135,6 +122,7 @@ public class LoginActivity extends Activity { //change to AuthNetActivityBase
         startActivity(startMain);
     }
     
+    /** Creates menu. */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
@@ -142,6 +130,7 @@ public class LoginActivity extends Activity { //change to AuthNetActivityBase
         return super.onCreateOptionsMenu(menu);
     }
 
+    /** Responds to each item on the menu. */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.dev_info) {
@@ -162,18 +151,18 @@ public class LoginActivity extends Activity { //change to AuthNetActivityBase
             return;
         }
 
-        // Reset errors.
+        /** Reset errors. */
         mLoginIDView.setError(null);
         mPasswordView.setError(null);
 
-        // Store values at the time of the login attempt.
+        /** Store values at the time of the login attempt. */
         mLoginID = mLoginIDView.getText().toString();
         mPassword = mPasswordView.getText().toString();
 
         boolean cancel = false;
         View focusView = null;
 
-        // Check for a valid password.
+        /** Check for a valid password. */
         if (TextUtils.isEmpty(mPassword)) {
             mPasswordView.setError(getString(R.string.error_field_required));
             focusView = mPasswordView;
@@ -184,7 +173,7 @@ public class LoginActivity extends Activity { //change to AuthNetActivityBase
             cancel = true;
         }
 
-        // Check for a valid log in ID.
+        /** Check for a valid log in ID. */
         if (TextUtils.isEmpty(mLoginID)) {
             mLoginIDView.setError(getString(R.string.error_field_required));
             focusView = mLoginIDView;
@@ -192,24 +181,24 @@ public class LoginActivity extends Activity { //change to AuthNetActivityBase
         }
 
         if (cancel) {
-            // There was an error; don't attempt login and focus the first
-            // form field with an error.
-    		final EditText errorEditText = (EditText) focusView;
+            /**  There was an error; don't attempt login and focus the first
+             * form field with an error. */
+            final EditText errorEditText = (EditText) focusView;
             focusView.requestFocus();
             errorEditText.setOnKeyListener(new OnKeyListener() {
-				@Override
-				public boolean onKey(View v, int keyCode, KeyEvent event) {
-            		if (keyCode == KeyEvent.KEYCODE_DEL) {
-            			errorEditText.setText("");
-            			return true;
-            		}
-					return false;
-				}
-            	
+                @Override
+                public boolean onKey(View v, int keyCode, KeyEvent event) {
+                    if (keyCode == KeyEvent.KEYCODE_DEL) {
+                        errorEditText.setText("");
+                        return true;
+                    }
+                    return false;
+                }
+                
             });
         } else {
-            // Show a progress spinner, and kick off a background task to
-            // perform the user login attempt.
+            /** Show a progress spinner, and kick off a background task to
+             * perform the user login attempt. */
             mLoginStatusMessageView.setText(R.string.login_progress_signing_in);
             showProgress(true);
             mAuthTask = new UserLoginTask();
@@ -220,9 +209,6 @@ public class LoginActivity extends Activity { //change to AuthNetActivityBase
     /** Shows the progress UI and hides the login form. */
     @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
     private void showProgress(final boolean show) {
-        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
-        // for very easy animations. If available, use these APIs to fade-in
-        // the progress spinner.
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
             int shortAnimTime = getResources().getInteger(
                     android.R.integer.config_shortAnimTime);
@@ -249,35 +235,31 @@ public class LoginActivity extends Activity { //change to AuthNetActivityBase
                         }
                     });
         } else {
-            // The ViewPropertyAnimator APIs are not available, so simply show
-            // and hide the relevant UI components.
             mLoginStatusView.setVisibility(show ? View.VISIBLE : View.GONE);
             mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
         }
     }
 
-    /** Represents an asynchronous login/registration task used to authenticate
+    /** An asynchronous login/registration task used to authenticate
      * the user. */
     public class UserLoginTask extends AsyncTask<Void, Void, Integer> {
-    	Result loginResult;
-    	public static final int RESULT_FAILURE           = -2;
+        Result loginResult;
+        public static final int RESULT_FAILURE           = -2;
         @Override
         protected Integer doInBackground(Void... params) {
-            // continue authenticate login
-        	authenticate(findViewById(R.id.login_form));
+            authenticate(findViewById(R.id.login_form));
             int loginCode;
             try {
-            	Transaction currentTransaction = _merchant.createMobileTransaction(TransactionType.MOBILE_DEVICE_LOGIN);
-            	MobileDevice mobileDevice = MobileDevice.createMobileDevice(deviceID, deviceInfo, deviceNumber);
-            	currentTransaction.setMobileDevice(mobileDevice);
-            	loginResult = (Result) _merchant.postTransaction(currentTransaction);
-            	loginCode = loginResult.isResponseOk() ? RESULT_OK : RESULT_FAILURE;
+                Transaction currentTransaction = _merchant.createMobileTransaction(TransactionType.MOBILE_DEVICE_LOGIN);
+                MobileDevice mobileDevice = MobileDevice.createMobileDevice(deviceID, deviceInfo, deviceNumber);
+                currentTransaction.setMobileDevice(mobileDevice);
+                loginResult = (Result) _merchant.postTransaction(currentTransaction);
+                loginCode = loginResult.isResponseOk() ? RESULT_OK : RESULT_FAILURE;
             } catch (Exception e) {
-            	loginCode = RESULT_FAILURE;
+                loginCode = RESULT_FAILURE;
             }
 
             try {
-                // Simulate network access.
                 Thread.sleep(2000);
             } catch (InterruptedException e) {
                 return RESULT_FAILURE;
@@ -291,14 +273,14 @@ public class LoginActivity extends Activity { //change to AuthNetActivityBase
             mAuthTask = null;
             showProgress(false);
             if (success == -1) {
-            	if (loginResult.isOk()) {
-            		String sessionToken = loginResult.getSessionToken();
-            		SessionTokenAuthentication sessionTokenAuthentication = SessionTokenAuthentication.createMerchantAuthentication(_merchant.getMerchantAuthentication().getName(), sessionToken, deviceID);
-            		_merchant.setMerchantAuthentication(sessionTokenAuthentication);
+                if (loginResult.isOk()) {
+                    String sessionToken = loginResult.getSessionToken();
+                    SessionTokenAuthentication sessionTokenAuthentication = SessionTokenAuthentication.createMerchantAuthentication(_merchant.getMerchantAuthentication().getName(), sessionToken, deviceID);
+                    _merchant.setMerchantAuthentication(sessionTokenAuthentication);
                     Intent chargeCardIntent = new Intent(LoginActivity.this, ChargeCardActivity.class);
                     startActivity(chargeCardIntent);
                     finish();
-            	}
+                }
 
             } else {
                 mPasswordView
@@ -314,14 +296,14 @@ public class LoginActivity extends Activity { //change to AuthNetActivityBase
         }
     }
 
-    // Utility functions for LoginActivity.java.
+    /** Utility functions for LoginActivity.java. */
 
     /** Dismisses the soft-key board outside of EditText area. */
     public static void hideSoftKeyboard(Activity activity) {
         InputMethodManager inputMethodManager = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
         inputMethodManager.hideSoftInputFromWindow(activity.getCurrentFocus().getWindowToken(), 0);
     }
-    
+
     /** Iterates through each View in this activity and checks if it is an
      * instance of EditText and if it is not, register a setOnTouchlistener
      * to that component. */
@@ -343,43 +325,35 @@ public class LoginActivity extends Activity { //change to AuthNetActivityBase
         }
     }
 
-	/** Displays a toast after the transaction has been processed and dismisses the keyboard. */
+    /** Displays a toast after the transaction has been processed and dismisses the keyboard. */
     public void displayToast(String message) {
-    	hideSoftKeyboard(this);
-    	Context currentContext = getApplicationContext();
-    	String text = message;
-    	int duration = Toast.LENGTH_SHORT;
-    	Toast completedTransactionToast = Toast.makeText(currentContext, text, duration);
-    	completedTransactionToast.setGravity(Gravity.CENTER_HORIZONTAL|Gravity.CENTER_VERTICAL, 0, 0);
-    	completedTransactionToast.show();
-	}
-    // Functions related to the SDK
-    
+        hideSoftKeyboard(this);
+        Context currentContext = getApplicationContext();
+        String text = message;
+        int duration = Toast.LENGTH_SHORT;
+        Toast completedTransactionToast = Toast.makeText(currentContext, text, duration);
+        completedTransactionToast.setGravity(Gravity.CENTER_HORIZONTAL|Gravity.CENTER_VERTICAL, 0, 0);
+        completedTransactionToast.show();
+    }
+
     /** Authenticates the user-name and password. */
     public void authenticate(View view) {
-    	// original authenticate sets deviceID, device telephone number and device description
-    //	if (checkNetwork(this)) {
-    		String loginID = ((EditText) findViewById(R.id.loginID)).getText().toString();
-    		String password = ((EditText) findViewById(R.id.password)).getText().toString();
-    		PasswordAuthentication loginCredentials = PasswordAuthentication.createMerchantAuthentication(loginID, password, deviceID);
-    		_merchant = Merchant.createMerchant(Environment.SANDBOX, loginCredentials);
-    		_merchant.setDuplicateTxnWindowSeconds(30);
-    	//} else {
-    	//	displayToast(getString(R.string.error_network));
-    	//}
+        String loginID = ((EditText) findViewById(R.id.loginID)).getText().toString();
+        String password = ((EditText) findViewById(R.id.password)).getText().toString();
+        PasswordAuthentication loginCredentials = PasswordAuthentication.createMerchantAuthentication(loginID, password, deviceID);
+        _merchant = Merchant.createMerchant(Environment.SANDBOX, loginCredentials);
+        _merchant.setDuplicateTxnWindowSeconds(30);
     }
-    
+
     /** Returns the NetworkInfo of the current context. */
     public static boolean checkNetwork(Context context) {
-    	ConnectivityManager cm = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
-    	NetworkInfo network = cm.getActiveNetworkInfo();
-    	if (network != null) { 
-    		return network.isAvailable();
-    	}
-    	return false;
-    	//NetworkInfo network = ((ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE)).getActiveNetworkInfo();
-    	//return network;
+        ConnectivityManager cm = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo network = cm.getActiveNetworkInfo();
+        if (network != null) { 
+            return network.isAvailable();
+        }
+        return false;
     }
-    
+
 }
 
