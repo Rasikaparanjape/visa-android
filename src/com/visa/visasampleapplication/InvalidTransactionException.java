4041d615a7ce5d000f3db630648a8b863d62afdd
Tricia Fu <triciasfu@gmail.com>
2014-07-03 14:47:51 -0700

    First Draft Done

--- src/com/visa/visasampleapplication/InvalidTransactionException.java
+++
@@ -1,8 +0,0 @@
-package com.visa.visasampleapplication;
-
-public class InvalidTransactionException extends Exception {
-	public InvalidTransactionException(String message) {
-		super(message);
-	}
-
-}
